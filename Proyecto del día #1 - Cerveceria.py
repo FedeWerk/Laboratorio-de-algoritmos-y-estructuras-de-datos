e267f5bbc84af6cd16b37b100c0bd60b67490880
FedeWerk <fedewerk@gmail.com>
2024-09-20 19:22:08 -0300

    Rename cerveceria.py to Proyecto del día #1 - Cerveceria.py

---
+++ Proyecto del día #1 - Cerveceria.py
@@ -0,0 +1,2 @@
+print("Te haré dos preguntas para poder armar el nombre de tu cerveza!")
+print('El nombre ideal para tu cerveza es:' + '"'+ input('¿Qué objeto viene a tu mente cuando digo "Amarillo"?') + ' ' + input('Dime un atributo que comience con la primer letra de tu nombre')+'"')
